be2631cf0e910d5f8834fcee91e5750d1753ce0d
bilal-windows <javaid.billal@gmail.com>
2022-08-03 11:57:34 +0500

    add solution and data files

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ rows=list()
 fname='output.csv'
 hfhandle = open("htmloutput.html", 'a+')
 
-strttag="<html> <head><style>table, th, td {border: 1px solid black;td {table-layout: fixed; width: 200px; height: 50px;vertical-align: bottom;}</style></head><body><table><tr><th>Sr.</th><th>File</th><th>Truth</th><th>Inference</th></tr>"
+strttag="<html> <head><style>table, th, td {border: 1px solid black;td {table-layout: fixed; width: 200px; height: 50px;vertical-align: bottom;}</style></head><body><table><tr><th>Sr.</th><th>File</th><th>Truth</th><th>Inference</th><th>CER</th><th>CER2</th></tr>"
 endtag = "</table></html>"
 
 hfhandle.write(strttag)
@@ -16,7 +16,7 @@ with open(fname,'r') as csvfile:
     fields = next(csvreader)
     for row in csvreader:
         img="<img src=\"./"+row[1]+"\" width=\"70%\" height=\"70%\" >"
-        htmlwrt= "<tr><td>"+row[0]+"</td><td>"+img +"</td><td>"+row[2]+"</td><td>"+row[3]+"</td></tr>"
+        htmlwrt= "<tr><td>"+row[0]+"</td><td>"+img +"</td><td>"+row[2]+"</td><td>"+row[3]+"</td><td>"+row[4]+"</td><td>"+row[5]+"</td></tr>"
         hfhandle.write(htmlwrt)
         hfhandle.write("\n")
     
